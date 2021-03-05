d08c62eef40415b13f43045e8f6e827cdede1f27
wuls <linsheng.wu@beantechs.com>
2021-03-05 17:26:37 +0800

    feat: 增加Eslint校验功能,保存时自动校验格式

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2020: true,
+    jest: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/typescript/recommended',
+    'eslint:recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ],
+  rules: {
+    'no-var': 'error',
+    'object-shorthand': 2,
+    'no-undef': 2,
+    camelcase: 'off',
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/ban-types': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
+    ]
+  }
+}

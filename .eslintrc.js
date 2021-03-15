7b641d3c1a679cc82ff2e21cc7fb02457424e592
wuls <linsheng.wu@beantechs.com>
2021-03-15 17:27:52 +0800

    fix: optimize eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   root: true,
   env: {
     node: true,
--- src/components/Col/src/Col.tsx
+++ src/components/Col/src/Col.tsx
@@ -1,17 +1,17 @@
-// import { defineComponent, computed } from 'vue'
-// import { props } from './props'
-// export default defineComponent({
-//   name: 'Col',
+import { defineComponent, computed } from 'vue'
+import { props } from './props'
+export default defineComponent({
+  name: 'Col',
 
-//   props,
+  props,
 
-//   setup(props, { slots }) {
-//     const classes = computed(()=>[])
-//     return () => (
-//       <div class={classes.value}>
-//         232
-//         {slots}
-//       </div>
-//     )
-//   }
-// })
+  setup(props, { slots }) {
+    const classes = computed(() => [])
+    return () => (
+      <div class={classes.value}>
+        23
+        {slots}
+      </div>
+    )
+  }
+})

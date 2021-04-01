66cc214b2a1f03f248387cafe5dd1ea6744d53af
wuls <linsheng.wu@beantechs.com>
2021-04-01 21:30:47 +0800

    feat: 创建rate组件

---
+++ src/components/Rate/src/Rate.tsx
@@ -0,0 +1,13 @@
+//styles
+import './style.scss'
+
+// js
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Rate',
+
+  setup() {
+    return () => {}
+  }
+})
---
+++ src/components/Rate/src/readme.md
@@ -0,0 +1,4 @@
+---
+title: Rate
+wrapperClass: md-Rate
+---
